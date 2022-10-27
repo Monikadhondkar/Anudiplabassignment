9b088c170b263560498a087d6d5143914c378109
Gangaram Daundkar <Gangaram Daundkar@DESKTOP-KHVH58G>
2022-10-27 14:16:43 +0530

    Labsession3

--- Main/src/shape/shape.java
+++ Main/src/shape/shape.java
@@ -1,19 +1,40 @@
 package shape;
  import java.util.*;
-		 class Shape
-		 {
-			 static final double PI=Math.PI;
-			 	void Areaofsquare(float a)
-			 	{
-			 		float A=a*a;
-			 		System.out.println("Area of the square:"+A);
+ class Shape
+ {
+ 	void Display() 
+ 	{
+ 		System.out.println("Shape...");
+ 	} 	
+ }
+ class Circle extends Shape
+ {
+ 	void area( )
+ 		{
+ 		double p = 3.14;
+ 		int r = 4;
+ 		double a = p*r*r;
+ 		System.out.println("Area of Circle= "+a+" sq.m");
+ 		}
+ 	
+ }
+ class Triangle extends Circle
+ {
+ 	void area1( )
+ 	{
+ 		int h = 4;
+ 		int b = 6;
+ 		double a = h*b/2;
+ 		System.out.println("Area of Triangle= " +a +"sq.m");
+ 	
+ 	}
+ 	public static void main(String[] args) 
+ 	{
+ 		Triangle t = new Triangle();
+ 		t.area();
+ 		t.area1();
+ 	}
+ }
+ 		
+ 	
 
-			 	}
-		 void Areaofcircle(double a)
-			{
-				double A=3.142*a*a;
-				System.out.println("Area of the Circle:"+A);
-
-			}
-		}
-		
