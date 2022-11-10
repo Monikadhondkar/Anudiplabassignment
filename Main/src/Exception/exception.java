8e119376e78e4dbadd8e4223891887e128f140f1
Gangaram Daundkar <Gangaram Daundkar@DESKTOP-KHVH58G>
2022-11-10 13:46:45 +0530

    Lab Assignment 5

---
+++ Main/src/Exception/exception.java
@@ -0,0 +1,18 @@
+package Exception;
+ 
+		class Zeromembererror  extends Exception  
+		{  
+		    public Zeromembererror (String str)  
+		    {  
+		        // calling the constructor of parent Exception  
+		        super(str);  
+		    }  
+		}  
+		    
+		
+
+	 
+	
+	
+
+
